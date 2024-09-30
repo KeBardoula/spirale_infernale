89a01aaadd39746be8aad24ddff12f12699dadc8
Kévin Bardoula <kevin@MacBook-Pro-de-Kevin.local>
2024-09-30 15:28:41 +0200

    calcul fibo

--- main.c
+++ main.c
@@ -1,14 +1,45 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
+
+unsigned long long fibonacci(int n){
+	if (n == 0) return 0;
+	if (n == 1) return 1;
+
+	unsigned long long a = 0, b = 1, temp;
+	for (int i = 2; i <= n; i++) {
+		temp = a + b;
+		a = b;
+		b = temp;
+	}
+
+	return b;
+}
+
+bool est_premier(unsigned long long n) {
+	if (n < 2) return false;
+	for (unsigned long long i = 2; i * i <= n; i++){
+		if (n % i == 0) return false;
+	}
+
+	return true;
+}
 
 int main(){
 	int barnabe = -36;
 	int barnabeH = 44;
-	int i = 0;
-	int n = 17713;
+	int fib_index = 22;
+	unsigned long long fib22, premier;
 
-	barnabeH = barnabeH + n;
+	fib22 = fibonacci(fib_index);
 
-	printf("Bernarbe est à %d m de hauteur", barnabeH);
-	exit(0);
+	premier = fib22 + 1;
+	do{
+		premier ++;
+	}while(!est_premier(premier));
+
+	barnabeH = barnabeH + premier;
+	printf("Bernabé est à %d mètre de hauteur.\n", barnabeH);
+
+	return 0;
 }
