31af6b6e096dd91e2ed1b433e1eeaa8a5f9c8c61
Kévin Bardoula <kevin@MacBook-Pro-de-Kevin.local>
2024-09-30 14:39:34 +0200

    Bernarbé : spirale infernale

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+	int barnabe = -36;
+	int barnabeH = 44;
+	int i = 0;
+	int n = 17713;
+
+	barnabeH = barnabeH + n;
+
+	printf("Bernarbe est à %d m de hauteur", barnabeH);
+	exit(0);
+}
